76971bcbfd99d6dc41d25d0e82df86fe8d54a552
Andrew Kaurov <56529033+Kaurov2000@users.noreply.github.com>
2020-02-15 18:37:58 +0500

    Add files via upload
    Окончательный вид DDL

--- rpms_ddl.sql
+++ rpms_ddl.sql
@@ -1,6 +1,8 @@
+DROP DATABASE IF EXISTS rpms;
 CREATE DATABASE rpms;
 USE rpms;
 
+-- Таблица, содержащая пользователей системы
 CREATE TABLE IF NOT EXISTS users (
 	user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	login VARCHAR(50) NOT NULL,
@@ -11,8 +13,8 @@ CREATE TABLE IF NOT EXISTS users (
 	PRIMARY KEY (user_id),
 	UNIQUE KEY (login)
 );
-CREATE INDEX users_login_idx ON users(login);
 
+-- Таблица профилей пользователей системы
 CREATE TABLE IF NOT EXISTS user_profiles (
 	user_id INT UNSIGNED NOT NULL,
 	first_name VARCHAR(100) NOT NULL,
@@ -25,15 +27,9 @@ CREATE TABLE IF NOT EXISTS user_profiles (
 	CONSTRAINT user_profiles_fk FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+-- Таблица с клиентами - арендаторами
 CREATE TABLE IF NOT EXISTS clients (
 	client_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	delete_time DATETIME,
-	PRIMARY KEY (client_id)
-);
-
-CREATE TABLE IF NOT EXISTS client_profiles (
-	client_id INT UNSIGNED NOT NULL,
 	first_name VARCHAR(100) NOT NULL,
 	middle_name VARCHAR(100),
 	last_name VARCHAR(100),
@@ -43,42 +39,165 @@ CREATE TABLE IF NOT EXISTS client_profiles (
 	comment TEXT,
 	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
 	update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT client_profiles_fk FOREIGN KEY (client_id) REFERENCES clients(client_id)
+	delete_time DATETIME,
+	PRIMARY KEY (client_id)
 );
-CREATE INDEX clients_name_idx ON clients(last_name,first_name,middle_name);
 
+
+-- Словарь услуг, которые могут оказывать объекты системы
+CREATE TABLE IF NOT EXISTS service_types (
+	service_type_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	service_type VARCHAR(120),
+	PRIMARY KEY (service_type_id)
+);
+
+-- Таблица с контрагентами
 CREATE TABLE IF NOT EXISTS contractors (
 	contractor_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	delete_time DATETIME,
-	PRIMARY KEY (contractor_id)
-);
-
-CREATE TABLE IF NOT EXISTS contractor_profiles (
-	contractor_id INT UNSIGNED NOT NULL,
 	name VARCHAR(100) NOT NULL,
 	country VARCHAR(100),
 	city VARCHAR(100),
 	address VARCHAR(200),
 	phone VARCHAR(15),
 	email VARCHAR(120),
-	service_type_id INT UNSIGNED,
 	comment TEXT,
 	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
 	update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT contractor_profiles_fk FOREIGN KEY (contractor_id) REFERENCES contractors(contractor_id)
+	delete_time DATETIME,
+	PRIMARY KEY (contractor_id)
 );
-CREATE INDEX contractor_profiles_name_idx ON contractor_profiles(name);
-CREATE INDEX contractor_profiles_service_type_id_idx ON contractor_profiles(service_type_id);
-CREATE INDEX contractor_profiles_city_idx ON contractor_profiles(country,city);
 
-CREATE TABLE IF NOT EXISTS service_types (
-	service_type_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	service_type VARCHAR(120),
-	PRIMARY KEY (service_type_id)
+-- Таблица связи контрагентов с оказываемыми услугами
+CREATE TABLE contractors_service_types_links (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	contractor_id INT UNSIGNED NOT NULL,
+	service_type_id INT UNSIGNED NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE KEY (contractor_id,service_type_id),
+	CONSTRAINT contractors_service_types_links_contractor_id_fk FOREIGN KEY (contractor_id) REFERENCES contractors(contractor_id),
+	CONSTRAINT contractors_service_types_links_service_type_id_fk FOREIGN KEY (service_type_id) REFERENCES service_types(service_type_id)
 );
-ALTER TABLE contractor_profiles 
-	ADD CONSTRAINT contractor_profiles_service_type_id_fk FOREIGN KEY (service_type_id) REFERENCES service_types(service_type_id);
-	
+
+
+-- Словарь типов объектов недвижимости
+CREATE TABLE asset_types (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	asset_type VARCHAR(50) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+-- Таблица с объектами недвижимости
+CREATE TABLE assets (
+	asset_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100),
+	country VARCHAR(100),
+	city VARCHAR(100),
+	address VARCHAR(200),
+	asset_type_id INT UNSIGNED,
+	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	delete_time DATETIME,
+	PRIMARY KEY (asset_id),
+	CONSTRAINT assets_asset_type_id_fk FOREIGN KEY (asset_type_id) REFERENCES asset_types(id)
+);
+
+-- Таблица связей пользователей и объектов недвижимости
+CREATE TABLE user_asset_links (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	user_id INT UNSIGNED NOT NULL,
+	asset_id INT UNSIGNED NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE KEY (user_id,asset_id),
+	CONSTRAINT user_asset_links_user_id_fk FOREIGN KEY (user_id) REFERENCES users(user_id),
+	CONSTRAINT user_asset_links_asset_id_fk FOREIGN KEY (asset_id) REFERENCES assets(asset_id)
+);
+
+
+-- Таблица связей арендаторов и объектов недвижимости
+CREATE TABLE client_asset_links (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	client_id INT UNSIGNED NOT NULL,
+	asset_id INT UNSIGNED NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE KEY (client_id,asset_id),
+	CONSTRAINT client_asset_links_client_id_fk FOREIGN KEY (client_id) REFERENCES clients(client_id),
+	CONSTRAINT client_asset_links_asset_id_fk FOREIGN KEY (asset_id) REFERENCES assets(asset_id)
+);
+
+-- Словарь типов документов. Документы относятся только к одному объекту
+CREATE TABLE document_types (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	document_type VARCHAR(50) NOT NULL,
+	document_subject_type VARCHAR(50) NOT NULL, 
+	PRIMARY KEY (id)
+);
+
+-- Таблица с документами
+CREATE TABLE documents (
+	document_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	owner_id INT UNSIGNED NOT NULL,
+	name VARCHAR(100),
+	document_number VARCHAR(100),
+	issue_date DATE,
+	document_type_id INT UNSIGNED,
+	document_subject_id INT UNSIGNED,
+	document_file VARCHAR(200) NOT NULL,
+	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	delete_time DATETIME,
+	PRIMARY KEY (document_id),
+	CONSTRAINT documents_document_type_id_fk FOREIGN KEY (document_type_id) REFERENCES document_types(id),
+	CONSTRAINT documents_owner_id_fk FOREIGN KEY (owner_id) REFERENCES users(user_id)
+);
+
+
+-- Словарь объектов системы, которые могут выступать сторонами договора
+CREATE TABLE counterpart_types (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	counterpart_type VARCHAR(50) NOT NULL, 
+	PRIMARY KEY (id)
+);
+
+
+-- Таблица с договорами
+CREATE TABLE contracts (
+	contract_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	name VARCHAR(200),
+	contract_number VARCHAR(100),
+	issue_date DATE,
+	user_id INT UNSIGNED NOT NULL,
+	asset_id INT UNSIGNED NOT NULL,
+	counterpart_type_id INT UNSIGNED,
+	counterpart_id INT UNSIGNED NOT NULL,
+	contract_file VARCHAR(200) NOT NULL,
+	service_type_id INT UNSIGNED,
+	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	delete_time DATETIME,
+	PRIMARY KEY (contract_id),
+	CONSTRAINT contracts_user_id_fk FOREIGN KEY (user_id) REFERENCES users(user_id),
+	CONSTRAINT contracts_asset_id_fk FOREIGN KEY (asset_id) REFERENCES assets(asset_id),
+	CONSTRAINT contracts_counterpart_type_id_fk FOREIGN KEY (counterpart_type_id) REFERENCES counterpart_types(id),
+	CONSTRAINT contracts_service_type_id_fk FOREIGN KEY (service_type_id) REFERENCES service_types(service_type_id)
+);
+
+
+-- Таблица со счетами
+CREATE TABLE bills (
+	bill_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	bill_number VARCHAR(100),
+	issue_date DATE,
+	contract_id INT UNSIGNED NOT NULL,
+	issuer_type_id INT UNSIGNED NOT NULL,
+	issuer_id INT UNSIGNED NOT NULL,
+	bill_amount DECIMAL(2),
+	payment_date DATE,
+	create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+	update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	delete_time DATETIME,
+	PRIMARY KEY (bill_id),
+	CONSTRAINT bills_contract_id_fk FOREIGN KEY (contract_id) REFERENCES contracts(contract_id),
+	CONSTRAINT bills_issuer_type_id_fk FOREIGN KEY (issuer_type_id) REFERENCES counterpart_types(id)
+);
 
 
