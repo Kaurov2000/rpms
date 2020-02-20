a1f260a688ee56f2bf78b21678cb7f0ae59c48f7
Andrew Kaurov <56529033+Kaurov2000@users.noreply.github.com>
2020-02-20 12:41:25 +0500

    Add files via upload
    client_overdue_payments is added

--- rpms_procedures.sql
+++ rpms_procedures.sql
@@ -16,17 +16,47 @@ BEGIN
 		'Insert cancelled: counterpart_id for expense contracts must reference contractor_service_type_links.id';
 	 END IF;
 END//
--- Проверка на наличие неоплаченных счетов. Функция возвращает id счета с самой
--- старой датой либо 0, если просроченных счетов по объекту нет
-DROP FUNCTION IF EXISTS overdue_payments //
-CREATE FUNCTION overdue_payments(input_asset_id INT)
+-- Проверка на наличие неоплаченных счетов. Функция возвращает колическтво
+-- просроченных счетов по объекту
+DROP FUNCTION IF EXISTS overdue_payments_count //
+CREATE FUNCTION overdue_payments_count(input_client_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE bill_id INT;
-	SELECT b.id INTO bill_id FROM bills b INNER JOIN contracts c 
-		ON b.contract_id = c.id AND c.asset_id = input_asset_id
+	SELECT count(b.id) INTO bill_id 
+	FROM bills b INNER JOIN contracts c ON b.contract_id = c.id
+		INNER JOIN contract_types ct ON ct.id =c.contract_type_id 
+			AND ct.contract_type = 'expense'
+		INNER JOIN assets a ON a.id = c.asset_id
+			AND a.client_id = input_client_id
 	WHERE b.due_date < CURRENT_DATE() AND b.payment_date is NULL
-	ORDER BY b.issue_date LIMIT 1;
 	RETURN coalesce(bill_id, 0);
 END//
+-- Процедура собирает таблицу с фактами просрочки платежа клиентами 
+DROP PROCEDURE IF EXISTS overdue_payments_log;
+CREATE PROCEDURE overdue_payments_log
+BEGIN
+	INSERT INTO client_overdue_payments 
+		(log_entry_date, user_id, asset_id, bill_id, overdue_days, client_id, client_name)
+	SELECT 
+		CURRENT_DATE() as log_entry_date,
+		a.user_id,
+		c.asset_id,
+		b.id as bill_id,
+		DATEDIFF(CURRENT_DATE(), b.due_date) as overdue_days,
+		cli.id as client_id,
+		CONCAT(cli.last_name, ' ', cli.first_name) as client_name
+	FROM bills b INNER JOIN contracts c ON b.contract_id = c.id 
+		INNER JOIN contract_types ct ON ct.id =c.contract_type_id 
+			AND ct.contract_type = 'income'
+		INNER JOIN clients cli ON c.counterpart_id = cli.id
+		INNER JOIN assets a ON a.id = c.asset_id 
+	WHERE b.due_date < CURRENT_DATE() AND b.payment_date is NULL
+END//
+
 DELIMITER ;
+
+
+	
+
+
